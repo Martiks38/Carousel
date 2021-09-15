99f2cf66cdc1eba4942f60a73edc5b4e2e9b6c47
Martiks38 <83650309+Martiks38@users.noreply.github.com>
2021-09-15 17:26:20 -0300

    Create carousel.js

---
+++ carousel.js
@@ -0,0 +1,69 @@
+function carousel(TIME = 8000){
+  
+  function prev(){
+    $slides[i].classList.remove('active');
+    $selector[i].classList.remove('active');
+   
+    i--;
+
+    if(i < 0) i = $slides.length - 1;    
+   
+    $slides[i].classList.add('active');
+    $selector[i].classList.add('active');
+ }
+
+ function next(){
+   $slides[i].classList.remove('active');
+   $selector[i].classList.remove('active');
+   
+   i++;
+   
+   if(i >= $slides.length) i = 0;
+   
+   $slides[i].classList.add('active');
+   $selector[i].classList.add('active');
+ }
+
+ const $slides = document.querySelectorAll('.slider-slide'),
+  $prevBtn = document.querySelector('.prev'),
+  $nextBtn = document.querySelector('.next'),
+  $selector = document.querySelectorAll('.selector');
+  
+  let nextSlide = setInterval(next, TIME);
+  
+  let i = 0;
+
+ document.addEventListener('click', (e)=>{
+  
+  if(e.target.matches('.selector')){
+   if(e.target.matches('.active')){
+    return false;
+   }else{
+    clearInterval(nextSlide);
+
+    $slides[i].classList.remove('active');
+    $selector[i].classList.remove('active');
+    
+    i = e.target.getAttribute('data-id');
+    
+    $slides[i].classList.add('active');
+    $selector[i].classList.add('active');
+    nextSlide = setInterval(next, TIME);
+   }
+  }
+
+  if(e.target === $prevBtn || e.target === $nextBtn){
+   clearInterval(nextSlide);
+
+   if(e.target === $prevBtn) prev();
+  
+   if(e.target === $nextBtn) next();
+
+   nextSlide = setInterval(next, TIME);
+  }
+ })
+}
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+ carousel();
+})
